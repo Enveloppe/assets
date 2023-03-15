bbbc263b527679ac968a00ff468bf5564ea3e127
Mara <mara-li@outlook.fr>
2023-03-15 09:46:50 +0100

    create gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+gulp.task('scripts', function() {
+    return gulp.src(['src/js/url_exist.js', 'src/js/*.js'])
+        .pipe(concat('javascript.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+    }
+);
+
+
+gulp.task("styles", function() {
+    return gulp.src("src/css/*.css")
+        .pipe(concat("styles.css"))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("dist"));
+});
+
+
+gulp.task('default', gulp.parallel('scripts', 'styles'));
